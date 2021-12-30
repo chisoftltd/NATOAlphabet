78f725fcd841876a5e4c095b54f395d72df720cb
Benjamin.Chinwe <chisoftltd@gmail.com>
2021-12-30 22:18:16 +0000

    Initial commit - NATO Alphabet Project

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+# TODO 1. Create a dictionary in this format:
+nato_data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+
+nato_data_list = {row.letter: row.code for (index, row) in nato_data.iterrows()}
+print(nato_data_list)
+
+
+{"A": "Alfa", "B": "Bravo"}
+
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+user_word = input("Enter a word: ").upper()
+user_nato_list = [nato_data_list[letter] for letter in user_word]
+print(user_nato_list)
